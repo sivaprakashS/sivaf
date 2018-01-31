d8c9ebbc445ac34e8c199b9f86093fecba2ee043
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-01-31 03:08:01 -0800

    sivafn.c

---
+++ sivafn.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int n,fact=1,i;
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+fact=fact*i;
+}
+printf("the factorial of given number is %d",fact);
+}
